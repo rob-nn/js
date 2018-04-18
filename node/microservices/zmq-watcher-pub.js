1e45b3b7fc7b55b52830d4940ccbe59fd6a096b7
rob-nn <aguiarlima.roberto@gmail.com>
2018-04-18 16:25:54 -0300

    l

---
+++ node/microservices/zmq-watcher-pub.js
@@ -0,0 +1,24 @@
+'use strict';
+
+console.log("Starting...");
+const fs = require('fs');
+console.log("Requesting zeromq");
+const zmq = require('zeromq');
+console.log("Getting file name...");
+const filename = prooess.argv[2];
+
+const publisher = zmq.socket('pub');
+fs.watch(filename, () => {
+	publisher.send(JSON.stringify({
+		type: 'changed',
+		file: filename,
+		timestamp: Date.now()
+	}));
+});
+
+publisher.bind('tcp://*:60400', err=> {
+	if (err) {
+		throw err;
+	}
+	console.log('Listening for zmq subscribers...');
+});
