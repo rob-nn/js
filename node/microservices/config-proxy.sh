b0c6603323ebad08e997a31c8b88bb62b5635922
rob-nn <aguiarlima.roberto@gmail.com>
2018-04-27 10:08:17 -0300

    xxxxx

--- node/microservices/config-proxy.sh
+++ node/microservices/config-proxy.sh
@@ -1,5 +1,5 @@
 npm config set cafile /home/node/app/wasp101.64.crt
-npm config set proxy http://10.0.2.2/3128
+npm config set proxy http://127.0.0.1:3128
 npm config set registry http://registry.npmjs.org/
 npm config set strict-ssl false
 npm config list
